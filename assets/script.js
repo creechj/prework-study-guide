cc76aee45abc17b38d3add3ab07814c7f28f2a5e
criatura435 <jmc435@gmail.com>
2023-03-15 20:40:58 -0500

    add JavaScript to project

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+/* var topic = "HTML";
+
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+} */
+
+var courses = ["HTML","CSS","Git","JavaScpipt"];
+
+for(x=0; x<courses.length; x++) {
+    console.log(courses[x]);
+}
